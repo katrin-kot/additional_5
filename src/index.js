85f67ac889fdaeb64fad95dbe106f11ede84953c
katrin-kot <enkor89@mail.ru>
2020-03-06 10:56:07 +0300

    feat: all tests are passing

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
-module.exports = check;
-function check(str, cfg) {
-  const arr = str.split("");
-  const opening = {};
-  for (let i =0; i<cfg.length;i++){
-   const config = cfg[i];
-   opening[config[0]]=config[1];
-  }
+module.exports = function check(str, bracketsConfig) {
+  const config = {};
+  bracketsConfig.forEach(item => {
+    config[item[0]] = item[1];
+  });
   const stack = [];
+  const arr = str.split('');
+  const flags = {};
   for (let i = 0; i < arr.length; i++) {
-    const elem = arr[i];
-    if (elem in opening && elem !==stack[stack.length-1]) {
-      stack.push(elem);
+    const el = arr[i];
+    if (el in config) {
+      if (config[el] !== el) {
+        stack.push(el);
+      } else {
+        flags[el] = !flags[el];
+        if (flags[el]) {
+          stack.push(el);
+        } else {
+          let popped = stack.pop();
+          if (popped === undefined) return false;
+          if (el !== config[popped]) return false; 
+        }
+      }
     } else {
-      if (stack.length === 0) {
-        return false;
-      }
-      const open = stack.pop();
-      const close = opening[open];
-      if (arr[i] !== close) {
-        return false;
-      }
+      let popped = stack.pop();
+      if (popped === undefined) return false;
+      if (el !== config[popped]) return false; 
     }
   }
-  if (stack.length === 0) {
-    return true;
-  }
+  if (stack.length === 0) return true;
   return false;
-};
+}
 
 
-const config4 = [['|', '|']];
 
-console.log(check('||', config4));
+
+
+
